82fff347a5b6000b31fcc2ba7cbc7ca7986497d7
sachin Jeevan <sachinjeevan2506@gmail.com>
2019-11-29 08:29:30 +0530

    day 3

--- sit8.c
+++ sit8.c
@@ -1,1 +1,40 @@
-//
+//x = {1,2,4,7,7,5,7}
+// max = 1
+// 2 max    max = 2
+// 4 max    max=4
+// 7 max    max =7
+// 7 max    max=7
+// 5 max    max = 7
+// 7 max    max = 7
+
+// count =0
+// max 1   count=0
+// max 2   count=0
+// max 4   count=0
+// max 7   count=1
+// max 7   count =2
+// max 5   count =2
+// max 7   count =3
+ind val
+0    1
+1    2
+2    4
+3    7
+4    7
+5    5
+6    7
+
+max =7
+
+int arr[8]
+
+ind  val
+
+0     0
+1     1
+2     1
+3     0
+4     1
+5     1
+6     0
+7     3
